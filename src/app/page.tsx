10839347df3c761652f7a484e7c3d67812f62ee9
dfdihsak <jessie.sui.js@gmail.com>
2024-08-27 13:44:28 -0700

    removed testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import { Faqs } from '@/components/Faqs'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
-import { Pricing } from '@/components/Pricing'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
-import { Testimonials } from '@/components/Testimonials'
+
 
 export default function Home() {
   return (
@@ -16,10 +15,8 @@ export default function Home() {
         <Hero />
         <PrimaryFeatures />
         <SecondaryFeatures />
-        <CallToAction />
-        <Testimonials />
-        <Pricing />
         <Faqs />
+        <CallToAction />
       </main>
       <Footer />
     </>
